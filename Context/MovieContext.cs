830d7e3321a2616e4007ce430d0ba82d3f0840f5
lcsmota <sacul_m@protonmail.com>
2022-12-01 09:25:06 -0300

    Add movie context

---
+++ Context/MovieContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MoviesApi.Models;
+
+namespace MoviesApi.Context;
+
+public class MovieContext : DbContext
+{
+    public MovieContext(DbContextOptions<MovieContext> options)
+        : base(options) { }
+
+    DbSet<Movie> Movies { get; set; } = null!;
+}
