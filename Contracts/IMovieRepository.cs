4f4b6c32af453d8807597adf8794cb0fd249b2d4
lcsmota <sacul_m@protonmail.com>
2022-12-01 09:55:13 -0300

    Fix return type

--- Contracts/IMovieRepository.cs
+++ Contracts/IMovieRepository.cs
@@ -6,7 +6,7 @@ public interface IMovieRepository
     Task<List<Movie>> GetMoviesAsync();
     Task<Movie> GetMovieByIdAsync(int id);
     Task<Movie> CreateMovieAsync(Movie movie);
-    Task UpdateMovieAsync(int id, Movie movie);
-    Task DeleteMovieAsync(int id);
+    Task<Movie> UpdateMovieAsync(int id, Movie movie);
+    Task<Movie> DeleteMovieAsync(int id);
 
 }
