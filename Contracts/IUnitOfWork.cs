0d4fc73348a7785a6b1b56831258959f86d957b8
lcsmota <sacul_m@protonmail.com>
2022-12-02 10:58:17 -0300

    Add star contract/interface in UOF interface

--- Contracts/IUnitOfWork.cs
+++ Contracts/IUnitOfWork.cs
@@ -3,5 +3,6 @@ namespace MoviesApi.Contracts;
 public interface IUnitOfWork
 {
     public IMovieRepository MoviesRepository { get; }
+    public IStarRepository StarsRepository { get; }
     Task Commit();
 }
